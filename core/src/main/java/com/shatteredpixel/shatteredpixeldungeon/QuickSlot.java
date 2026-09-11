661f6077e35f93d23eef101780d8daf34c97ea5a
morrowminders <114840079+morrowminders@users.noreply.github.com>
2026-09-11 10:03:06 -0400

    Update QuickSlot.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/QuickSlot.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/QuickSlot.java
@@ -5,6 +5,9 @@
  * Shattered Pixel Dungeon
  * Copyright (C) 2014-2024 Evan Debenham
  *
+ * 13 Tribes Pixel Dungeons
+ * Cooyright (C) 2024-2026 M.V. Morrow
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
